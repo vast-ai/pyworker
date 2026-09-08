5dc8eb2e50f9ef67412ad87ea087a4665ea1cd91
Rob Ballantyne <rob@vast.ai>
2026-09-08 12:14:22 +0100

    Don't require HF_TOKEN in start_server.sh
    HF_TOKEN is only needed for gated models.

--- start_server.sh
+++ start_server.sh
@@ -89,7 +89,6 @@ function install_vastai_sdk() {
     fi
 }
 
-[ -n "$BACKEND" ] && [ -z "$HF_TOKEN" ] && report_error_and_exit "HF_TOKEN must be set when BACKEND is set!"
 [ -z "$CONTAINER_ID" ] && report_error_and_exit "CONTAINER_ID must be set!"
 [ "$BACKEND" = "comfyui" ] && [ -z "$COMFY_MODEL" ] && report_error_and_exit "For comfyui backends, COMFY_MODEL must be set!"
 
